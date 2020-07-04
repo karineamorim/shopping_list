bcd62e2e4dcf0836661caf091df993f9bdb38978
Andre Oliveira <desmonplays@gmail.com>
2020-07-04 17:27:31 -0400

    feature: spec validating order to bem greater than or equal to zero

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -4,9 +4,12 @@ RSpec.describe List, type: :model do
   context 'validate presence of' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:order) }
+    it do
+      should validate_numericality_of(:order).is_greater_than_or_equal_to(0)
+    end
   end
 
   context 'validate associations' do
-    it { should has_many(:products) }
+    it { should have_many(:products) }
   end
 end
