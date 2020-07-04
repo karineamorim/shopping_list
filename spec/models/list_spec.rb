46d835d91e12c9037b2fcd95e69c9944f5b4e8f5
Andre Oliveira <desmonplays@gmail.com>
2020-07-04 17:22:26 -0400

    feature: validating association has many products

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe List, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:order) }
   end
+
+  context 'validate associations' do
+    it { should has_many(:products) }
+  end
 end
