a947a0e81214ef08d93c9b015543b063575a0d01
Andre Oliveira <desmonplays@gmail.com>
2020-07-04 17:27:47 -0400

    feature: spec validating price to bem greater than zero

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Product, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:price) }
     it { should validate_presence_of(:list_id) }
+    it { should validate_numericality_of(:price).is_greater_than(0) }
   end
 
   context 'validate associations' do
