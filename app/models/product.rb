fa0fc5946f2ea711025496c8980852df521797ed
Andre Oliveira <desmonplays@gmail.com>
2020-07-04 17:14:20 -0400

    fix: additional necessary comma

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord 
-  validates :name, :price, :list_id presence: true
+  validates :name, :price, :list_id, presence: true
   validates :price, numericality: { greater_than: 0 }
 end
